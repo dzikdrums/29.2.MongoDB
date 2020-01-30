673bbe674943c9df913ae75ff98422ed36bc1c5d
Maciej Dzik <dzikdrums@gmail.com>
2020-01-30 09:36:06 +0100

    add mongoose

---
+++ models/employees.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const employeesSchema = new mongoose.Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  department: {type: String, required: true}
+  },
+  {
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model('Employees', employeesSchema);
