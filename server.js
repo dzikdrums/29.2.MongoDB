52a506fbe0fbe67dccfe4d7c86530855417eeaf4
Maciej Dzik <dzikdrums@gmail.com>
2020-02-05 13:55:00 +0100

    add endpoints tests

--- server.js
+++ server.js
@@ -28,11 +28,9 @@ app.use((req, res) => {
 mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
-db.once('open', () => {
-  console.log('Connected to the database');
-});
+db.once('open', () => {});
 db.on('error', err => console.log('Error ' + err));
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
-});
+const server = app.listen('8000', () => {});
+
+module.exports = server
